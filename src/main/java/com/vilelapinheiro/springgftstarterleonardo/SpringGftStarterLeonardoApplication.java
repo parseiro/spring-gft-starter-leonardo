55ee7839c242cc9752d16033585916858ed206f7
Leonardo Vilela Pinheiro <leonardo@vilelapinheiro.com>
2022-05-30 21:10:29 -0300

    Projeto inicializado com Spring Initializr

---
+++ src/main/java/com/vilelapinheiro/springgftstarterleonardo/SpringGftStarterLeonardoApplication.java
@@ -0,0 +1,13 @@
+package com.vilelapinheiro.springgftstarterleonardo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringGftStarterLeonardoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringGftStarterLeonardoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/vilelapinheiro/springgftstarterleonardo/SpringGftStarterLeonardoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vilelapinheiro.springgftstarterleonardo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringGftStarterLeonardoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
